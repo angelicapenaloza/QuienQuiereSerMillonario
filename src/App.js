9366f19025129512348704182581afad8b757fa4
Angelica Penaloza <ampenalozag@unal.edu.co>
2018-10-10 12:47:21 -0700

    adicione score y sonido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import { questions } from './questions.json';
 import './App.css';
+import right from './right.mp3';
+import wrong from './wrong.mp3';
 
-const T = (props) => {
-   return( <h1>{props.questions}</h1>)
+function score(){
+  let score = document.getElementById("score")
+  score.innerHTML = parseInt(score.innerHTML) + 1000000
 }
+
+class T extends Component {
+  constructor(props){
+    super(props)
+
+
+  }
+
+  render(){
+   return( <h4 onClick={()=> this.clicked()}>{this.props.question.content[this.props.choice]}</h4>)
+  }
+  clicked() {
+    console.log(this.props)
+    var audio;
+    if (this.props.question.correct === this.props.choice){
+      audio = new Audio(right);
+      score()
+    } else {
+      audio = new Audio(wrong);
+    }
+
+    audio.play()
+  }
+}
+
 
 
 class App extends Component {
@@ -15,6 +43,7 @@ class App extends Component {
 
 
     }
+
 
 
   clicked(){
@@ -36,21 +65,31 @@ class App extends Component {
 
 
             <header>
+            <br />
               <img src={require('./logo.jpg')} height="230"  onClick={ ()=> this.clicked() }/>
+              <br /><br />
+              <div id="score">500000</div>
             </header>
+<br /><br />
+                  <h4> {this.state.questions[this.state.question].question}</h4>
+                  <br />
+                  <T  question={this.state.questions[this.state.question]} choice={0}/>
+                  <T  question={this.state.questions[this.state.question]} choice={1}/>
+                  <T  question={this.state.questions[this.state.question]} choice={2}/>
+                  <T  question={this.state.questions[this.state.question]} choice={3}/>
 
-            <T questions={this.state.questions[this.state.question].question}/>
-            <T questions={this.state.questions[this.state.question].content[0]}/>
-            <T questions={this.state.questions[this.state.question].content[1]}/>
-            <T questions={this.state.questions[this.state.question].content[2]}/>
-            <T questions={this.state.questions[this.state.question].content[3]}/>
 
+
+<br />
+<br />
+<br />
 
                 <footer>
                   <img src={require('./Llamada.png')} height="50"/>
                   <img src={require('./Audience.png')} height="50"/>
                   <img src={require('./Classic5050.png')} height="50" />
                 </footer>
+
       </div>
     );
   }
